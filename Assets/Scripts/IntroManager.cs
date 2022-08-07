9c2ee79d81db4c5ea7bd99c28ef1030f50b86269
Atanu Debnath <playatanu@gmail.com>
2022-08-07 08:14:22 +0530

    up

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    public int introSize;
+
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu(introSize));
+    }
+
+
+
+    IEnumerator LoadMainMenu(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        SceneLoader.instance.LoadScene("MainMenu");
+    }
+}
